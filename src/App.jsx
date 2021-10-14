16df1ec2bc5d1af9fe6d5f38501d849e5083e013
tnakamura0314 <70365627+tnakamura0314@users.noreply.github.com>
2021-10-14 14:30:25 +0900

    htmlの大枠の作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+              <p>TODO1</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p>TODO2</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <div>
+              <p>TODOだったよ</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
